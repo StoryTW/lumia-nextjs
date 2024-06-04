47c9ea44ac235dccf0345a347b96fb0a14d3de75
parienko_sergej <parienko_sergej@enot.work>
2024-06-05 01:51:32 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,25 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Poppins } from 'next/font/google';
 import { ReactNode } from 'react';
 import { Footer } from '@/layouts/Footer/Footer';
 import { Header } from '@/layouts/Header/Header';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import '@/assets/styles/index.scss';
 
-const popins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'] });
+const popins = Poppins({ subsets: ['latin'], weight: ['400', '500'] });
 
 export const metadata: Metadata = {
   title: 'Lumia',
   description: 'Lumia',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
--- src/pageComponents/MainPage/MainPage.tsx
+++ src/pageComponents/MainPage/MainPage.tsx
@@ -1,10 +1,49 @@
+'use client';
 import { MainSection } from '@/components/sections/MainSection/MainSection';
 import styles from './MainPage.module.scss';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Navigation, Pagination } from 'swiper/modules';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+import { useLayoutEffect } from 'react';
+import gsap from 'gsap';
 
 export function MainPage() {
+  useLayoutEffect(() => {
+    gsap.registerPlugin(useGSAP, ScrollTrigger);
+  }, [])
+  
+
   return (
     <main id='mainPage' className={styles.mainPage}>
       <MainSection />
     </main>
   );
 }
+
+// <main id='mainPage' className={styles.mainPage}>
+//   <Swiper
+//     loop={false}
+//     autoplay={false}
+//     // pagination={{
+//     //   clickableClass: styles.swiper_pagination,
+//     //   bulletClass: styles.bullet,
+//     //   bulletActiveClass: styles.activeBullet,
+//     //   clickable: true,
+//     // }}
+//     modules={[Pagination, Navigation]}
+//     // onBeforeInit={(swiper) => {
+//     //   swiperRef.current = swiper;
+//     // }}
+//   >
+//     <SwiperSlide>
+//       <h1>FIRST</h1>
+//     </SwiperSlide>
+//     <SwiperSlide>
+//       <MainSection />
+//     </SwiperSlide>
+//     <SwiperSlide>
+//       <h1>SECOND</h1>
+//     </SwiperSlide>
+//   </Swiper>
+// </main>
